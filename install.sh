28b5e62c85d8f70110217ea370fdfa50e19776fa
Dylan Neve <dylanneve1@gmail.com>
2019-12-03 11:52:58 +0000

    Fix some errors and add cloning.

--- install.sh
+++ install.sh
@@ -6,5 +6,15 @@ sleep 1
 echo Installing Java.
 sudo apt update > /dev/null 2>&1
 sudo apt upgrade > /dev/null 2>&1
-sudo apt install latest-jre > /dev/null 2>&1
+sudo apt install -y default-jdk > /dev/null 2>&1
 echo - Java installed.
+echo
+java --version
+echo
+echo Setting up server files.
+echo - Downloading... this may take some time so be patient.
+git clone https://gitlab.com/dylanneve1/mc-server-nonjava-gitlab.git > /dev/null 2>&1
+echo - Downloaded files.
+mv mc-server-nonjava-gitlab server-files
+echo - Renamed folder.
+echo
