7036142418504b09c7a661258090f95c04fbc3e8
dylanneve1 <dylanneve1@gmail.com>
2019-12-03 18:01:50 +0000

    Fix some errors with installer and improve

--- install.sh
+++ install.sh
@@ -1,28 +1,34 @@
+# Script by dylanneve1@gmail.com
+
+RED='\033[1;31m'
+CYAN='\033[0;36m'
+NC='\033[0m'
 echo
-echo Welcome to MAKEOUTHILL MC Server installer
-echo By Dylan Neve
+echo "${RED}    ----------------------------------------------"
+echo "${RED}    - Welcome to MAKEOUTHILL MC Server installer -"
+echo "${RED}    -               By Dylan Neve                -"
+echo "${RED}    ----------------------------------------------"
 echo
 sleep 1
-echo Installing Java.
+echo "${CYAN}    Installing Java."
 sudo apt update > /dev/null 2>&1
-sudo apt upgrade > /dev/null 2>&1
+sudo apt upgrade -y > /dev/null 2>&1
+echo "${NC}       - Everything is up to date"
 sudo apt install -y default-jdk > /dev/null 2>&1
-echo - Java installed.
+echo "${NC}       - Java installed."
 echo
-java --version
-echo
-echo Setting up server files.
-echo - Downloading... this may take some time so be patient.
+echo "${CYAN}    Setting up server files."
+echo "${NC}       - Downloading... this may take some time so be patient."
 git clone https://gitlab.com/dylanneve1/mc-server-nonjava-gitlab.git > /dev/null 2>&1
-echo - Downloaded files.
+echo "${NC}       - Downloaded files."
 mv mc-server-nonjava-gitlab server-files
-echo - Renamed folder.
+echo "${NC}       - Renamed folder."
 echo
-echo Setting up screen.
+echo "${CYAN}    Setting up screen."
 sudo apt install -y screen > /dev/null 2>&1
-echo - Finshed setting up screen.
+echo "${NC}       - Finshed setting up screen."
 echo
-echo Done installing.
-echo Please run 'sudo ./start.sh' to continue.
+echo "${RED}    Done installing."
+echo "${RED}    Please run 'sudo ./start.sh' to continue."
 echo
 sleep 2
