71ac68d92af5a07e3585950664f79fe81b78b3ae
Dylan Neve <dylanneve1@gmail.com>
2019-12-03 11:37:59 +0000

    Add basic installer script beginning and Java

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo
+echo Welcome to MAKEOUTHILL MC Server installer
+echo By Dylan Neve
+echo
+sleep 1
+echo Installing Java.
+sudo apt update > /dev/null 2>&1
+sudo apt upgrade > /dev/null 2>&1
+sudo apt install latest-jre > /dev/null 2>&1
+echo - Java installed.
